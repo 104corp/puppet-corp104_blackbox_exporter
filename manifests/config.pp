24f682f6c787e94bf2127e15bab47ae9fe74d4c2
Martin.Lan <Martin.Lan@MartinLan-MBPR.local>
2020-04-21 15:39:59 +0800

    test

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,7 @@ class corp104_blackbox_exporter::config (
     notify  => Class['corp104_blackbox_exporter::service']
   }
 
-  if "${corp104_blackbox_exporter::config_dir}" {
+  if $corp104_blackbox_exporter::config_dir {
     file { "${corp104_blackbox_exporter::config_dir}":
       ensure => directory,
       before => File["${config_path}"],
