29442e7c060b682f9feb67dbacae33542cfcd3df
Martin.Lan <Martin.Lan@MartinLan-MBPR.local>
2020-04-21 14:31:11 +0800

    test

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,5 @@
 class corp104_blackbox_exporter::config (
   String $monitor_type,
-  String $config_path,
 ) inherits corp104_blackbox_exporter {
   File {
     owner   => 'root',
@@ -9,7 +8,7 @@ class corp104_blackbox_exporter::config (
     notify  => Class['corp104_blackbox_exporter::service']
   }
 
-  file { '/opt/blackbox_exporter-0.16.0.linux-amd64/blackbox.yml':
+  file { "${corp104_blackbox_exporter::config_path}":
     ensure  => file,
     content => template("${module_name}/blackbox_exporter.yaml.erb"),
   }
